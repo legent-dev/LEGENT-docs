de45fa78f3323cefeca7df44753e4ffbac754065
cheng <chengzl18@163.com>
2025-02-20 17:59:00 +0800

    Initial commit

---
+++ copy_assets.py
@@ -0,0 +1,6 @@
+import shutil
+
+shutil.copy("assets/robot-light.svg", "LEGENT/docs/robot-light.svg")
+shutil.copy("assets/robot-dark.svg", "LEGENT/docs/robot-dark.svg")
+shutil.copy("assets/LEGENT-logo-transparent.webp", "LEGENT/docs/LEGENT-logo.webp")
+shutil.copy("assets/index.md", "LEGENT/docs/en/index.md")
